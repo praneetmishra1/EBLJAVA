6034ada276e1296fbcd631d0d530f0eb2b050425
username <wecp-dummy@gmail.com>
2024-02-13 04:45:48 +0000

    wecp initial commit

--- frontend/src/app/bank/types/tstypes/Accountts.ts
+++ frontend/src/app/bank/types/tstypes/Accountts.ts
@@ -1,15 +1,14 @@
-export class AccountTS{
+export class AccountTS {
     accountId?: string;
     customerId: string;
     balance: number;
-
-    constructor(customerId:string, balance: number, accountId?: string){
+    constructor( customerId: string, balance: number,accountId?:string,) {
         this.accountId = accountId;
         this.customerId = customerId;
         this.balance = balance;
     }
 
-    displayInfo(){
+    displayInfo() {
         console.log(`Account ID: ${this.accountId}`);
         console.log(`Customer ID: ${this.customerId}`);
         console.log(`Balance: ${this.balance.toFixed(2)}`);
